4115710b37c138adba0f3f8fa4cb9d2f193f4f8a
Abdoulrachard <Abdoulrachard@gmail.com>
2024-06-17 01:28:20 +0100

    add cdn to base file

--- resources/views/Base/baseRl.blade.php
+++ resources/views/Base/baseRl.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="http://localhost/bootstrap/dist/css/bootstrap.min.css">
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
     <link rel="stylesheet" href="http://localhost/fontawesome/css/all.min.css">
     <script src="http://localhost/bootstrap/dist/js/bootstrap.bundle.js"></script>
@@ -17,5 +18,7 @@
        
         @yield('content')
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
 </body>
 </html>

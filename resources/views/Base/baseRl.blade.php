1314758283303123072fe2e5b2f9a2271c7313f6
Abdoulrachard <Abdoulrachard@gmail.com>
2024-07-17 00:59:33 +0100

    add google analitics tag

--- resources/views/Base/baseRl.blade.php
+++ resources/views/Base/baseRl.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9HTXFZ4EK6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9HTXFZ4EK6');
+</script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
